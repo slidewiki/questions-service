fc41147eeb90da5b4da5b725a72ca3d14c5edc98
Luis Fernandes <sednanref2004@gmail.com>
2017-06-01 11:30:30 +0200

    created question model.

---
+++ application/models/question.js
@@ -0,0 +1,48 @@
+/**
+ * Created by lfernandes on 01.06.17.
+ */
+'use strict';
+
+//require
+let Ajv = require('ajv');
+let ajv = Ajv({
+    verbose: true,
+    allErrors: true
+}); // options can be passed, e.g. {allErrors: true}
+
+//build schema
+const question = {
+    type: 'object',
+    properties: {
+        _id: {
+            type: 'number'
+        },
+        related_object: {   // "deck" / "slide" / ... (?)
+            type: 'string'
+        },
+        related_object_id: {   // id of the related object
+            type: 'number'
+        },
+        question: {
+            type: 'string'
+        },
+        user_id: {
+            type: 'string' //TODO check lowercase
+        },
+        choices: {
+            type: 'array',
+            items: {
+              type: 'string'
+            }
+        },
+        difficulty: {
+            type: 'number',
+            minimum: 1,
+            maximum: 5
+        }
+    },
+    required: ['_id', 'related_object', 'related_object_id', 'question', 'user_id', 'choices']
+};
+
+//export
+module.exports = ajv.compile(question);
