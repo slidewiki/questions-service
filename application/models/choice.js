75ce447c84b88c0a4969a2fdab10a685d9b5c38e
Luis Fernandes <sednanref2004@gmail.com>
2017-06-01 11:34:47 +0200

    Added choice model.

---
+++ application/models/choice.js
@@ -0,0 +1,37 @@
+/**
+ * Created by lfernandes on 01.06.17.
+ */
+/**
+ * Created by lfernandes on 01.06.17.
+ */
+'use strict';
+
+//require
+let Ajv = require('ajv');
+let ajv = Ajv({
+    verbose: true,
+    allErrors: true
+}); // options can be passed, e.g. {allErrors: true}
+
+//build schema
+const choice = {
+    type: 'object',
+    properties: {
+        _id: {
+            type: 'number'
+        },
+        choice: {
+            type: 'string'
+        },
+        is_correct: {
+            type: 'boolean' //TODO check lowercase
+        },
+        explanation: {
+            type: 'string'
+        }
+    },
+    required: ['_id', 'choice', 'is_correct']
+};
+
+//export
+module.exports = ajv.compile(choice);
