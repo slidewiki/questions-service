38cf4f3cbac9ede4225dd2c79d8343687042e556
Luis Fernandes <sednanref2004@gmail.com>
2017-06-07 15:27:58 +0200

    Modified model of questions and created database handler for the collection.

---
+++ application/database/questionDatabase.js
@@ -0,0 +1,67 @@
+/**
+ * Created by lfernandes on 07.06.17.
+ */
+/*
+ Controller for handling mongodb and the data model questions while providing CRUD'ish.
+ */
+
+'use strict';
+
+const helper = require('./helper'),
+  questionModel = require('../models/question.js');
+
+module.exports = {
+  get: function (identifier) {
+    return helper.connectToDatabase()
+      .then((db) => db.collection('quesions'))
+      .then((col) => col.findOne({
+        _id: identifier
+      }));
+  },
+
+  insert: function (question) {
+    //TODO check for root and parent deck ids to be existent, otherwise create these
+    return helper.connectToDatabase()
+      .then((db) => helper.getNextIncrementationValueForCollection(db, 'questions'))
+      .then((newId) => {
+        // console.log('newId', newId);
+        return helper.connectToDatabase() //db connection have to be accessed again in order to work with more than one collection
+          .then((db2) => db2.collection('questions'))
+          .then((col) => {
+            let valid = false;
+            question._id = newId;
+            try {
+              valid = questionModel(question);
+              // console.log('validated questionModel', valid);
+              if (!valid) {
+                return questionModel.errors;
+              }
+              return col.insertOne(question);
+            } catch (e) {
+              console.log('validation failed', e);
+            }
+            return;
+          }); //id is created and concatinated automatically
+      });
+  },
+
+  replace: function (id, question) {
+    return helper.connectToDatabase()
+      .then((db) => db.collection('questions'))
+      .then((col) => {
+        let valid = false;
+        try {
+          valid = questionModel(question);
+          if (!valid) {
+            return questionModel.errors;
+          }
+          return col.findOneAndReplace({
+            _id: id
+          }, question);
+        } catch (e) {
+          console.log('validation failed', e);
+        }
+        return;
+      });
+  }
+};
--- application/models/question.js
+++ application/models/question.js
@@ -6,56 +6,56 @@
 //require
 let Ajv = require('ajv');
 let ajv = Ajv({
-    verbose: true,
-    allErrors: true
+  verbose: true,
+  allErrors: true
 }); // options can be passed, e.g. {allErrors: true}
 
 //build schema
 const question = {
-    type: 'object',
-    properties: {
-        _id: {
-            type: 'number'
-        },
-        related_object: {   // "deck" / "slide" / ... (?)
-            type: 'string'
-        },
-        related_object_id: {   // id of the related object
-            type: 'number'
-        },
-        question: {
-            type: 'string'
-        },
-        user_id: {
-            type: 'string' //TODO check lowercase
-        },
-        choices: {
-            type: 'array',
-            items: {
-                type: 'object',
-                properties: {
-                    _id: {
-                        type: 'number'
-                    },
-                    choice: {
-                        type: 'string'
-                    },
-                    is_correct: {
-                        type: 'boolean' //TODO check lowercase
-                    },
-                    explanation: {
-                        type: 'string'
-                    }
-                }
-            }
-        },
-        difficulty: {
-            type: 'number',
-            minimum: 1,
-            maximum: 5
-        }
+  type: 'object',
+  properties: {
+    _id: {
+      type: 'number'
     },
-    required: ['_id', 'related_object', 'related_object_id', 'question', 'user_id', 'choices']
+    related_object: {   // "deck" / "slide" / ... (?)
+      type: 'string'
+    },
+    related_object_id: {   // id of the related object
+      type: 'number'
+    },
+    question: {
+      type: 'string'
+    },
+    user_id: {
+      type: 'string' //TODO check lowercase
+    },
+    choices: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          _id: {
+            type: 'number'
+          },
+          choice: {
+            type: 'string'
+          },
+          is_correct: {
+            type: 'boolean' //TODO check lowercase
+          },
+          explanation: {
+            type: 'string'
+          }
+        }
+      }
+    },
+    difficulty: {
+      type: 'number',
+      minimum: 1,
+      maximum: 5
+    }
+  },
+  required: ['_id', 'related_object', 'related_object_id', 'question', 'user_id', 'choices']
 };
 
 //export
